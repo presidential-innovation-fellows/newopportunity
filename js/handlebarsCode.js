bc3a03d56d40bb9188089ec833b05a42f7ff8a75
Mark Brown II <mbrownii@doc.gov>
2016-09-27 15:58:42 -0400

    Add styles to gallery items to improve user experience

--- js/handlebarsCode.js
+++ js/handlebarsCode.js
@@ -4,13 +4,13 @@ $.ajax({
    dataType: 'json',
    success: function( data ) {
       
-
+      $(document).ready( function() {
       // Gallery items build
       var thumbnailSource = $('#galleryTemplate').html();
       var thumbnailTemplate = Handlebars.compile(thumbnailSource);
       var thumbnailHTML = thumbnailTemplate(data);
-      $('#gallery-opp-proj').append(thumbnailHTML);
-      console.log(data);
+      $('#gallery-opp-proj').prepend(thumbnailHTML);
+      })
 
    }
 })
